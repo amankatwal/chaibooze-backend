8aacae1173446c54628066ab3eb124f66fb620cf
Amandeep Singh <katwal004@Gmail.com>
2026-06-10 16:53:24 +0530

    New Changes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(helmet({
 app.use(morgan());
 app.use(cors(
     {
-        origin : [process.env.VITE_FRONTEND_URL, process.env.VITE_DASHBOARD_URL],
+        origin : [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization']
